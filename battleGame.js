bd77a1fede65def7dfc0272bd732d2a3828cfb33
RojasPablo <147354067+RojasPablo@users.noreply.github.com>
2023-10-31 16:47:13 -0700

    HW 3 completed

--- battleGame.js
+++ battleGame.js
@@ -1,14 +1,35 @@
 // 1. Create attack function below.  This will take the following parameters:
 // attackingPlayer, defendingPlayer, baseDamage, variableDamage
-
+function attack(attackingPlayer, defendingPlayer, baseDamage, variableDamage){
+  const randomDamage = Math.floor(Math.random() * (variableDamage + 1))
+  console.log(randomDamage)
+  totalDamage = randomDamage + baseDamage
+  defendingPlayer.health -= randomDamage
+  return `${attackingPlayer.name} hits ${defendingPlayer.name} for ${totaleDamage} damage. ${defendingPlayer.name} has ${defendingPlayer.health} health remaining`
+}
 
 
 // 2. Create player1 and player2 objects below
 // Each should have a name property of your choosing, and health property equal to 10
+const player1 = {
+  health: 10,
+  name: 'Mario'
+}
+const player2 = {
+  health: 10,
+  name: 'Bowser'
+}
 
+console.log(attack(player1,player2,1,4))
 
 
 // 3. Refactor attack function to an arrow function.  Comment out function above.
+const attack = (attackingPlayer, defendingPlayer, baseDamage, variableDamage) => {
+  const randomDamage = math.floor(math.random() * (variableDamage + 1))
+  totaleDamage = randomDamage + baseDamage
+  defendingPlayer.health -= randomDamage
+  return `${attackingPlayer.name} hits ${defendingPlayer.name} for ${totaleDamage} damage. ${defendingPlayer.name} has ${defendingPlayer.health} health remaining`
+}
 
 
 
